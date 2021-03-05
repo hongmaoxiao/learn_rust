d92f6b191999ab8b44ace2008b609c8654c03a58
hongmaoxiao <769501294@qq.com>
2021-03-05 10:27:15 +0800

    add slice more

--- chapter4/slice/src/main.rs
+++ chapter4/slice/src/main.rs
@@ -6,6 +6,36 @@ fn main() {
 
     s.clear();
     println!("s now: {}!", s);
+
+
+    let s1 = String::from("hello world");
+
+    let word = first_word1(&s1);
+
+    // s1.clear();
+
+    println!("first wordld: {}", word);
+
+
+
+    let my_string = String::from("hello world");
+    let my_string1 = String::from("hello world");
+    let my_string_literal = "hello world";
+
+
+    let word2 = first_word2(&my_string);
+
+    let word3 = first_word2(&my_string1[..]);
+
+    let word4 = first_word2(&my_string_literal[..]);
+
+    let word5 = first_word2(my_string_literal);
+
+
+    println!("word2: {}", word2);
+    println!("word3: {}", word3);
+    println!("word4: {}", word4);
+    println!("word5: {}", word5);
 }
 
 fn first_word(s: &String) -> usize {
@@ -18,4 +48,28 @@ fn first_word(s: &String) -> usize {
     }
 
     s.len()
+}
+
+fn first_word1(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn first_word2(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for(i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
